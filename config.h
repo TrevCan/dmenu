8f0e6bc7b44d2196d402df50784cefc524fde537
Hector Canizales <heror0484@protonmail.com>
2021-12-06 18:42:23 -0600

    add center patch

--- config.h
+++ config.h
@@ -1,18 +1,19 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-/* -fn option overrides fonts[0]; default X11 font or font set */
+static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */    /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
+	"JetBrainsMono:pixelsize=50:antialias=true:autohint=true:style=bold",
 	"monospace:size=10",
-	"JoyPixels:pixelsize=8:antialias=true:autohint=true"
 };
 static const unsigned int bgalpha = 0xe0;
 static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeNorm] = { "yellow", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
@@ -31,3 +32,6 @@ static unsigned int lines      = 0;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+// mon config avec 
+//fonts dmenu_run -b -nf yellow -fn JetBrainsMono:pixelsize=50:style=bold 
